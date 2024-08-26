ab92c0c6ade4ae1ec0e70df313737d66bdf8a745
ky1015 <kylie.struhs@gmail.com>
2024-08-25 19:25:38 -0600

    added week2 work

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -12,10 +12,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         <nav class="navbar">
           <ul>
             <li><a routerLink="/">Home</a></li>
-            <li><a href="#">#</a></li>
-            <li><a href="#">#</a></li>
-            <li><a href="#">#</a></li>
-            <li><a href="#">#</a></li>
+            <li><a routerLink="/players">Players</a></li>
+            <li><a routerLink="/signin">Sign In</a></li>
+            <li><a routerLink="/create-character">Create Character</a></li>
+            <li><a routerLink="/create-guild">Create Guild</a></li>
+            <li><a routerLink="/character-faction">Character Faction</a></li>
           </ul>
         </nav>
         <section class="content">
@@ -23,10 +24,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         </section>
       </main>
       <footer class="footer">
-        <nav class="footer-nav">
-          <a routerLink="/">Home</a> | <a href="#">#</a> |
-          <a href="#">#</a> | <a href="#">#</a> |
-          <a href="#">#</a>
+           <nav class="footer-nav">
+            <a routerLink="/">Home</a> |
+            <a routerLink="/players">Players</a> |
+            <a routerLink="/signin">Sign In</a> |
+            <a routerLink="/create-character">Create Character</a> |
+            <a routerLink="/create-guild">Create Guild</a>
+            <a routerLink="/character-faction">Character Faction</a>
         </nav>
         <p>&copy; 2024 CharacterVerse</p>
       </footer>
@@ -35,3 +39,4 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styles: [``],
 })
 export class AppComponent {}
+
